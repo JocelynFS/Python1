370f20454b477fe714aab20bd87f261b4fa09923
Jocelyn Francisco S <jocelyn.fco.s@gmail.com>
2022-03-27 15:56:27 -0600

    Agregué el archivo .py.

---
+++ python1.py
@@ -0,0 +1,7 @@
+def suma(x, y):
+    return x+y
+
+print('Función que suma dos números')
+x=int(input('Ingrese el primer número: '))
+y=int(input('Ingrese el segundo número: '))
+print(f'El resultado de x={x} y y={y} es {suma(x, y)}')
